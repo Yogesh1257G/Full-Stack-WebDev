ac7b1412dadaa474fdd5bd5a9db9abed62210ef3
Yogesh Soni <ysoni8672@gmail.com>
2026-07-13 10:47:02 +0530

    Add ExpressJS

---
+++ ExpressJS/Request And Response/main.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const blog = require('./routes/blog')
+const shop = require('./routes/shop')
+ 
+
+
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+app.use('/blog', blog)
+app.use('/shop', shop)
+
+app.get('/', (req, res) => {
+    console.log("Hey its a get request")
+    res.send('Hello World21!')
+}).post('/', (req, res) => {
+    console.log("Hey its a post request")
+    res.send('Hello World post!')
+})
+// Can be tested using http://localhost:3000/mypage.html
+// Create new data when user enters something
+
+app.put('/', (req, res) => {
+    console.log("Hey its a put request")
+    res.send('Hello World put!')
+})
+// Update the existing data
+
+app.get("/index", (req, res) => {
+    console.log("Hey its index")
+    res.sendFile('templates/index.html', { root: __dirname })
+})
+
+app.get("/api", (req, res) => {
+    res.json({ a: 1, b: 2, c: 3, d: 4, name: ["harry", "jerry"] })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
